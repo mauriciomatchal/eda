52c4036705d3313b267bd3dee7c359561cf05285
mauriciomatchal <98425176+mauriciomatchal@users.noreply.github.com>
2023-05-05 13:30:06 +0000

    modified:   eda mauricio/ex 002.py

--- eda mauricio/ex 002.py
+++ eda mauricio/ex 002.py
@@ -1,19 +1,19 @@
 class Node:
     def __init__(self, value):
         self.value = value
-        self.prox = None    
+        self.prox = None
     def show_node(self):
-        return (f'{self.value}')
+        return f'{self.value}'
 
 class LinkedList:
     def __init__(self):
-        self.head = None  
-    def Insert(self, x):
+        self.head = None
+    def Insert(self, node):
         if self.head is not None:
-            x.prox = self.head
-            self.head = x
+            node.prox = self.head
+            self.head = node
         else:
-            self.head = x
+            self.head = node
 
     def Append(self, x):
         if self.head is not None: # checks if the list is not empty
@@ -22,13 +22,11 @@ class LinkedList:
                 if temp.prox is None: # if the list has only one element
                     temp.prox = x # the next element is "x"
                     break
-                else:
-                    temp = temp.prox 
-                    # if there is another element after 1 the temp now points to this number
+                temp = temp.prox
+                # if there is another element after 1 the temp now points to this number
         else:
-            self.head = x 
-            # if the list is empty the first is x
-            
+            self.head = x
+            # if the list is empty the first is x           
     def show(self):
         if self.head is not None:
             temp = self.head
