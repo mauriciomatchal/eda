609adb987facd8cd44f710ea58d8572e1b644c9c
mauriciomatchal <98425176+mauriciomatchal@users.noreply.github.com>
2023-05-05 19:10:22 +0000

    modified:   eda mauricio/aula 001.py 	modified:   eda mauricio/ex 002.py

--- eda mauricio/aula 001.py
+++ eda mauricio/aula 001.py
@@ -1,42 +1,35 @@
 
 class Node:
-
     def __init__(self, value):
         self.value = value
-    
     def show(self):
-        return(f'Valor: {self.value}')
-
+        return f'Valor: {self.value}'
+    
 class StaticList:
     def __init__(self, size):
         self.current_size = 0
         self.maximum_size = size
         self.list = [None] * size #creates an empty list
-
-
-    def insert(self, x):
+    def insert(self, node):
         if self.current_size < self.maximum_size: #checks if the list is not full
             #stores a node of the type node that has a "self.value" "x" within itself
 
-            self.list[self.current_size] = Node(x) #stores the
+            self.list[self.current_size] = Node(node) #stores the node value
             self.current_size += 1
         else:
             print('List is full')
-
     def show(self):
         for x in range(self.current_size):
             print(f'{x}: {self.list[x].show()}')
 
 
 def main():
-    l = StaticList(3)
-    
-    l.insert(1)
-    l.insert(2)
-    l.insert(3)
-    l.insert(4)
-    
-    l.show()
+    lista = StaticList(3)
+    lista.insert(1)
+    lista.insert(2)
+    lista.insert(3)
+    lista.insert(4)
+    lista.show()
 
 if __name__ == '__main__':
     main()
--- eda mauricio/ex 002.py
+++ eda mauricio/ex 002.py
@@ -8,14 +8,13 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-    def Insert(self, node):
+    def insert_first(self, node):
         if self.head is not None:
             node.prox = self.head
             self.head = node
         else:
             self.head = node
-
-    def Append(self, x):
+    def append_last(self, x):
         if self.head is not None: # checks if the list is not empty
             temp = self.head # creates a pointer to the first element called "temp"
             while True:
@@ -26,7 +25,39 @@ class LinkedList:
                 # if there is another element after 1 the temp now points to this number
         else:
             self.head = x
-            # if the list is empty the first is x           
+            # if the list is empty the first is x
+    def free_list(self):
+        if self.head is not None:
+            temp = self.head
+            while True:
+                if temp.prox is not None:
+                    temp.prox = None 
+                    self.head = None
+
+                    print('Free list now')
+                    break    
+                else:
+                    temp = None
+        else:
+            return 'List is already empty'
+    def size_of(self):
+        temp = self.head
+        size = 0
+        if self.head is None:
+            print('List is empty')
+        else:
+            while True:
+                if temp.prox is not None:
+                    size += 1
+                    temp = temp.prox
+                else:
+                    size += 1
+                    break
+            print(size)
+                
+
+
+
     def show(self):
         if self.head is not None:
             temp = self.head
@@ -39,12 +70,16 @@ def main():
 
     lista = LinkedList()
 
-    lista.Append(Node(2))
-    lista.Append(Node(3))
-    lista.Append(Node(5))
-
+    lista.append_last(Node(2))
+    lista.append_last(Node(3))
+    lista.append_last(Node(5))
+    lista.append_last(Node(6))
     lista.show()
 
+    #lista.free_list()
+    #lista.show()
+
+    lista.size_of()
 
 if __name__ == '__main__':
     main()
