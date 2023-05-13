09c59475eeac260689a90e3432f7ec9692700452
mauriciomatchal <98425176+mauriciomatchal@users.noreply.github.com>
2023-05-13 17:23:19 +0000

    modified:   lab 1/string/ex007.cpp

--- lab 1/string/ex007.cpp
+++ lab 1/string/ex007.cpp
@@ -7,11 +7,8 @@ int main() {
     cin >> size;
     char block = '#';
     char sky = '>';
-
     for (int i = 1; i <= size; i++) {
         cout << string(size - i, sky) << string(i, block) << endl;
     }
-
-
     return 0;
 }
