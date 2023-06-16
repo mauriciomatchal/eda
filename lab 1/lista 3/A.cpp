768f3799669bd865b935a4625b53fd2c5d41af7a
mauriciomatchal <98425176+mauriciomatchal@users.noreply.github.com>
2023-06-16 18:57:43 +0000

    new file:   lab 1/lista 3/Quanto menor Melhor.cpp 	new file:   lab 1/lista 3/A.cpp

---
+++ lab 1/lista 3/A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+    float max = num/2;
+    float floor(max);
+	if (num == 0) {
+		return 0;
+	} else if (num == 2 || num == 3 || num == 5) {
+	    cout << "Seu numero é primo";
+	} else {
+        int i;
+	    int verify = 0;
+        while (i < max) {
+            int rem = num % i
+        }
+        }
+        if(verify == 0) {
+            cout << "Seu numero nao é primo";
+        }
+    }
+
+	return 0;
+}
