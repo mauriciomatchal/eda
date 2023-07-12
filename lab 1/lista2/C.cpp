56015f402f3fa2286d6d5f53f6f1a2bd6737cb20
mauriciomatchal <98425176+mauriciomatchal@users.noreply.github.com>
2023-07-12 23:14:09 +0000

    modified:   lab 1/lista2/C.cpp

--- lab 1/lista2/C.cpp
+++ lab 1/lista2/C.cpp
@@ -1,43 +1,65 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
 
 int main() {
-    int sum = 0, amount = 0, prev, max, min, num;
+    int sum = 0, amount = 0, num = 0, media = 0, min = 10, max = 1;
+    vector<int> resultados;
     while(true) {
         string entrada;
         cin >> entrada;
-        
-        if(entrada == "FIM") {
+
+        if (entrada == "FIM") {
             if (amount > 0) {
-                int media = sum / amount;
-                cout << media << endl;
-                
+                if (amount == 1) {
+                    media = num;
+                    resultados.push_back(media);
+                } else if (amount == 2) {
+                    media = sum / 2;
+                    resultados.push_back(media);
+                } else {
+                    media = (sum - min - max) / (amount - 2);
+                    resultados.push_back(media);
+                }
                 sum = 0;
                 amount = 0;
+                min = 10;
+                max = 1;
             }
             
             string entrada2;
             cin >> entrada2;
             
-            if(entrada2 == "FIM") {
-                return 0;
+            if (entrada2 == "FIM") {
+                break;
             } else {
-                int num = stoi(entrada2);
+                num = stoi(entrada2);
+                if (num < min) {
+                    min = num;
+                }
+                if (num > max) {
+                    max = num;
+                }
                 sum += num;
-                amount += 1;
+                amount ++;
             }
         } else {
-            if (num < stoi(entrada)) {
+            num = stoi(entrada);
+            if (num < min) {
                 min = num;
-            } else {
+            }
+            if (num > max) {
                 max = num;
             }
-            num = stoi(entrada);
             sum += num;
-            amount += 1;
+            amount ++;
         } 
     }
+
+    for (const int& x : resultados) {
+        cout << x << endl;
+    }
+    
     return 0;
 }
